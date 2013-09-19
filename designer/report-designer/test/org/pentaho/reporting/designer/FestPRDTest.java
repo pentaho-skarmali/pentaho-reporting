1e2f4f8251ff8a327dae35f0a5461fcdae12e706
Sulaiman Karmali <skarmali@pentaho.com>
2013-09-19 13:25:24 -0400

    Able to double-click on IF formula

--- designer/report-designer/test/org/pentaho/reporting/designer/FestPRDTest.java
+++ designer/report-designer/test/org/pentaho/reporting/designer/FestPRDTest.java
@@ -20,56 +20,30 @@
 package org.pentaho.reporting.designer;
 
 import java.awt.Component;
-import java.awt.Dialog;
-import java.awt.Point;
 import javax.swing.JComboBox;
 import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import junit.framework.TestCase;
-import org.fest.swing.annotation.GUITest;
 import org.fest.swing.annotation.RunsInEDT;
 import org.fest.swing.core.BasicComponentFinder;
-import org.fest.swing.core.BasicRobot;
 import org.fest.swing.core.ComponentFinder;
-import org.fest.swing.core.ComponentMatcher;
 import org.fest.swing.core.GenericTypeMatcher;
-import org.fest.swing.core.MouseClickInfo;
-import org.fest.swing.core.Robot;
 import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
-import org.fest.swing.edt.GuiTask;
-import org.fest.swing.exception.ComponentLookupException;
-import org.fest.swing.fixture.CommonComponentFixture;
-import org.fest.swing.fixture.ComponentFixture;
-import org.fest.swing.fixture.ContainerFixture;
 import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.fixture.FrameFixture;
 import org.fest.swing.fixture.JComboBoxFixture;
-import org.fest.swing.fixture.JComponentFixture;
 import org.fest.swing.fixture.JListFixture;
-import org.fest.swing.fixture.JPanelFixture;
-import org.fest.swing.fixture.JPopupMenuFixture;
-import org.fest.swing.fixture.MouseInputSimulationFixture;
-import org.fest.swing.fixture.WindowFixture;
-import org.fest.swing.timing.Timeout;
-import org.jfree.chart.JFreeChart;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.pentaho.openformula.ui.DefaultFunctionParameterEditor;
-import org.pentaho.openformula.ui.FieldDefinition;
 import org.pentaho.openformula.ui.FormulaEditorDialog;
 import org.pentaho.openformula.ui.FormulaEditorModel;
 import org.pentaho.openformula.ui.FormulaEditorPanel;
-import org.pentaho.openformula.ui.MultiplexFunctionParameterEditor;
 import org.pentaho.reporting.designer.core.DefaultReportDesignerContext;
 import org.pentaho.reporting.designer.core.ReportDesignerBoot;
 import org.pentaho.reporting.designer.core.ReportDesignerFrame;
-import org.pentaho.reporting.designer.core.editor.report.ResizeRootBandComponent;
-import org.pentaho.reporting.designer.core.editor.report.RootBandRenderComponent;
-import org.pentaho.reporting.designer.core.util.DesignerFormulaEditorDialog;
 import org.pentaho.reporting.designer.core.util.GUIUtils;
 import org.pentaho.reporting.designer.testsupport.TestReportDesignerView;
 import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
@@ -84,15 +58,15 @@ public class FestPRDTest extends TestCase
   {
   }
 
-  private void block()
+  private void block(final long secs)
   {
     try
     {
-      Thread.sleep(10000000);
+      Thread.sleep(secs * 1000);
     }
     catch (InterruptedException e)
     {
-      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+      e.printStackTrace();
     }
   }
 
@@ -158,10 +132,10 @@ public class FestPRDTest extends TestCase
 //    Object element = (Object)finder.findByType(JFreeChart.class);
 
 
-    block();
+    block(10);
   }
 
-//  @GUITest     // takes screenshot if test case fails
+  //  @GUITest     // takes screenshot if test case fails
   @RunsInEDT
   @Test
   public void testFormulaEditorDialog() throws XulException, InterruptedException
@@ -175,16 +149,6 @@ public class FestPRDTest extends TestCase
     FormulaEditorPanel panel = (FormulaEditorPanel)formulaEditorDialog.createContentPane();
     JTextArea formulaTextArea = panel.getFunctionTextArea();
     FormulaEditorModel model = panel.getEditorModel();
-
-
-    // Retrieve formula category drop-down and select the 2nd item in list
-    final JComboBox categoryComboBox = (JComboBox)finder.findByName(dialog, "categoryComboBox");
-    final JList formulaList = (JList)finder.findByName(dialog, "formulaList");
-    System.out.println("******* formulaList: " + formulaList.getName() + " with element at  " + formulaList.getModel().getElementAt(2));
-    System.out.println("\t size = " + formulaList.getModel().getSize());
-
-    final JComboBoxFixture JComboBoxFixture = new JComboBoxFixture(window.robot, categoryComboBox);
-    final JListFixture jListFixture = new JListFixture(window.robot, formulaList);
 
 
     GenericTypeMatcher genericTypeMatcher = new GenericTypeMatcher(FormulaEditorDialog.class, false)
@@ -206,87 +170,18 @@ public class FestPRDTest extends TestCase
     fixture.requireModal();
 
 
+    final JComboBox categoryComboBox = (JComboBox)finder.findByName(dialog, "categoryComboBox");
+    final JComboBoxFixture comboBoxFixture = new JComboBoxFixture(window.robot, categoryComboBox);
+    comboBoxFixture.selectItem("Logical");
 
+    // Since we selected a new item, get a refreshed list
+    final JList filteredFormulaList = (JList)finder.findByName(dialog, "formulaList");
+    final JListFixture filteredListFixture = new JListFixture(window.robot, filteredFormulaList);
 
-////    dialog.show();
-//
-    GuiActionRunner.execute(new GuiTask() {
-      public void executeInEDT() {
-        System.out.println("** selecting item ");
+    // Select IF formula
+    filteredListFixture.doubleClickItem("IF");
 
-//        categoryComboBox.setSelectedIndex(2);
-//        formulaList.setSelectedIndex(2);
-//
-////        dialog.setVisible(true);
-//        jListFixture.selectItem(2).doubleClick();
-      }
-    });
-
-    Thread.sleep(5000);
+    block(15);
     fixture.button("OK").click();
-
-    block();
-//
-//    GuiActionRunner.execute(new GuiTask() {
-//      public void executeInEDT()
-//      {
-//        System.out.println("Double click");
-//        jListFixture.doubleClick();
-//      }
-//    });
-//
-//
-//    dialogFixture.button("OK").click();
-
-//    categoryComboBox.setSelectedIndex(1);
-//    System.out.println("Item count: " + categoryComboBox.getItemCount());
-//    System.out.println("first item: " + categoryComboBox.getItemAt(1));
-//    System.out.println("******** categroy combo: " + categoryComboBox.toString());
-
-
-
-//    ComponentFinder finder = window.robot.finder();
-//    DesignerFormulaEditorDialog editorDialog = new DesignerFormulaEditorDialog(window.component());
-
-//    DialogFixture fixture = new DialogFixture(dialog);
-//    fixture.rightClick();
-//
-//    dialog.show();
-
-
-//    dialog.editFormula("=COUNT()", new FieldDefinition[0]);
-
-//    fixture.show();
-
-//    FormulaEditorPanel formulaEditorPanel = (FormulaEditorPanel)dialog.createContentPane();
-//    formulaEditorPanel.setFormulaText("=COUNT(1;2;3)");
-  }
-
-  @Test
-  public void validateFormulaEditor()
-  {
-    FormulaEditorPanel panel = new FormulaEditorPanel();
-    final Robot robot = BasicRobot.robotWithCurrentAwtHierarchy();
-
-//    ComponentFixture<FormulaEditorPanel> fixture = new Comp onentFixture<FormulaEditorPanel>(robot, panel);
-
-    panel.getFunctionTextArea().getDocument().removeDocumentListener(panel.getDocSyncHandler());
-
-    panel.setFormulaText("=COUNT()");
-
-    MultiplexFunctionParameterEditor functionParameterEditor = panel.getFunctionParameterEditor();
-    DefaultFunctionParameterEditor activeEditor = functionParameterEditor.getDefaultEditor();
-
-    activeEditor.fireParameterUpdate(0, "SUM(1)");
-    activeEditor.fireParameterUpdate(1, "2");
-    activeEditor.fireParameterUpdate(2, "3");
-
-    assertEquals("=COUNT(SUM(1);2;3)", panel.getFormulaText());
-  }
-
-  @Test
-  public void testSplashScreen()
-  {
-    window.focus();
   }
 }
